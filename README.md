7c365b39b5513556c4a16770e486ff568f15a2cd
Alex Vieira <lexvieira.info@gmail.com>
2021-11-25 01:46:28 +0700

    Update Googletrans Free API

--- README.md
+++ README.md
@@ -3,4 +3,17 @@ Multi Translator for learners of many languages, being possible to identify the 
 
 Comming Soon
 
+Using the version from Github  git clone https://github.com/lushan88a/google_trans_new.git 
+Removed from the project and updated with the version below
+    git rm -r --cached app/google_trans_new/
+    git commit -m "Remove Google Trans Git and add to Gitignore"
+    git push -u origin main
+
+Updated for the version:
+googletrans = "==4.0.0-rc1"
+
+Translation for text with context using Cloud Translation API will be available in the next version
+
+google-cloud-translate = "==2.0.1"
+
 Improvements, mobile app and controllers with functions more organized
--- app/pyTranslateMulti.py
+++ app/pyTranslateMulti.py
@@ -1,16 +1,17 @@
 #Using Google Translator to Translate words and sentences.
-from app.google_trans_new.google_trans_new import google_translator  
+from googletrans import Translator  
 import gtts
 import base64
 def translate(text):
     if text != "":
+        translator = Translator(service_urls=[
+            'translate.google.com'
+        ])
+        
         to_translate = text
         # print(text)
         
         translations = []    
-        # translations["text"] = text
-
-        translator = google_translator()
         
         lang_list = {"en": "English","pt-br":"Portuguese BR","th": "Thai", "es": "Spanish", "fr": "French", "pl": "Polish", "it": "Italian"}
         language = translator.detect(to_translate)
@@ -19,17 +20,28 @@ def translate(text):
         language_to_translate = { language[0] : language[1] }
         filename = "translation_lang.mp3"
         for k, l in lang_list.items():
-            translate_text = translator.translate(to_translate,lang_src='auto',lang_tgt=k)  
+            lang_dest = k
+            lang_src = "auto"
+            a = translator.translate(to_translate, dest=lang_dest, src = lang_src)            
             
-            tts = gtts.gTTS(translate_text, lang=k) 
+            print("text: {}, translate: {}, pronunciation: {}".format(text, a.text, a.pronunciation))
+            translation_return = translator.translate(to_translate,lang_src='auto',lang_tgt=k)  
+            
+            translation_text = "{}".format(a.text) 
+            p = translation_return.pronunciation
+            
+            tts = gtts.gTTS(translation_text, lang=k) 
             tts.save(filename.replace("_lang","_" + k))
 
             with open(filename.replace("_lang","_" + k), "rb") as f1:
                 base64encode = base64.b64encode(f1.read()).decode('ascii')
                 f1.close()
-            
+
+            if p != None:
+                translation_text = "{} ({})".format(translation_text, p)
+                            
             # return basereturn
-            translations.append({"lang_code": k,"language": l, "translation": translate_text, "audio": "data:audio/mpeg;base64," + str(base64encode) })
+            translations.append({"lang_code": k,"language": l, "translation": translation_text, "audio": "data:audio/mpeg;base64," + str(base64encode) })
         
         return { "text": text, "language": language_to_translate, "translations": translations}, 200
     else: 
