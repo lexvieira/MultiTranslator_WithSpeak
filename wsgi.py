5b42821adac4a3ea120b8b3b2e4dbd4322621f3b
Alex Vieira <lexvieira.info@gmail.com>
2023-05-08 16:07:15 +0700

    Docker Gcloud Configuration

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 from app.main import app
 import os
 # app.run(host='0.0.0.0', port=os.environ.get('PORT', default=5000))
-if __name__ == 'main':
-	app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+if __name__ == "__main__":
+  app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080))) 
